a3b0d4afab5a11566d98e16f89449eeb522b2e9b
Hollywood-Kid <tisinnikita352@gmail.com>
2024-11-27 22:33:49 +0300

    server update

--- internal/handler/view/home/index_templ.go
+++ internal/handler/view/home/index_templ.go
@@ -8,6 +8,8 @@ package home
 import "github.com/a-h/templ"
 import templruntime "github.com/a-h/templ/runtime"
 
+import "github.com/Hollywood-Kid/InfoSecChallenge/internal/handler/view/layout"
+
 func Index() templ.Component {
 	return templruntime.GeneratedTemplate(func(templ_7745c5c3_Input templruntime.GeneratedComponentInput) (templ_7745c5c3_Err error) {
 		templ_7745c5c3_W, ctx := templ_7745c5c3_Input.Writer, templ_7745c5c3_Input.Context
